ffc108766a5139e1ca581ba78bba4acfadfe2ffa
Jeff Pruyne <jeffgpi3@gmail.com>
2020-12-28 14:46:25 -0500

    debugging role_claimer

--- role_claimer.js
+++ role_claimer.js
@@ -1,5 +1,5 @@
 Creep.prototype.init_claimer = function(){
-    if(this.memory.room_target === undefined){
+    if(this.memory.target_room === undefined){
         console.log(`${this.name} is missing a target claim`)
     }
     this.memory.target = null
@@ -10,14 +10,14 @@ Creep.prototype.cleanup_claimer = function(){
 }
 
 Creep.prototype.claimer_role = function(){
-    if(this.room.name === this.memory.room_target){
+    if(this.room.name === this.memory.target_room){
         if(this.claimController(this.room.controller) === ERR_NOT_IN_RANGE){
             this.moveTo(this.room.controller)
         }
     } else {
         if(this.memory.target === undefined || this.memory.target === null || this.memory.target.roomName !== this.room.name){
-            const find_exit = this.findExitTo(this.memory.room_target)
-            this.memory.target = this.pos.findClosestByPath(find_exit)
+            const find_exit = this.room.findExitTo(this.memory.target_room)
+            this.memory.target = this.pos.findClosestByPath(this.room.find(find_exit))
         }
         this.moveTo(this.memory.target.x, this.memory.target.y)
     }
